e4ba5c68e2046a65830853f082eff6ca67442517
JAMES-HP <u1257434@unimail.hud.ac.uk>
2017-03-28 10:37:12 +0100

    initial commit

---
+++ src/main/java/com/uoh/NetworkrailsysGatherAppApplication.java
@@ -0,0 +1,12 @@
+package com.uoh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkrailsysGatherAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkrailsysGatherAppApplication.class, args);
+	}
+}
